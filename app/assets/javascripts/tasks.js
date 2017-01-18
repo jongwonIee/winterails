1e37410c885317fdb0da99f94f25641c5e2f6dad
csy1204 <josang1204@gmail.com>
2017-01-18 04:54:37 +0000

    first commit

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,21 @@
+/* global $ */
+/* global jQuery */
+jQuery.fn.submitOnCheck = function() {
+    this.find('input[type=submit]').remove();
+    this.find('input[type=checkbox]').click(function(){
+      $(this).parent('form').submit(); 
+    });
+    return this;
+};
+
+$(function() {
+    $('.edit_task').submitOnCheck();
+});
+
+
+// $(function() {
+//     $('.edit_task input[type=submit]').remove();
+//     $('.edit_task input[type=checkbox]').click(function(){
+//       $(this).parent('form').submit(); 
+//     });
+// });
