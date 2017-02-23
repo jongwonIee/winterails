15818f355628f3dcd7961daa710041c8d15503f4
Dong-geon Lim <ooqwe486@gmail.com>
2017-02-23 07:31:22 +0000

    Update Permit

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -69,6 +69,6 @@ class ProductTypesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_type_params
-      params.require(:product_type).permit(:name, fields_attributes: [:field_type, :name, :required])
+      params.require(:product_type).permit(:name, fields_attributes: [:field_type, :name, :required, :_destroy, :id])
     end
 end
